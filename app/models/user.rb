41cf40d8e5b3ce323b8bedc9d825423f531e049d
Susan Prestage <susan.prestage@gmail.com>
2013-07-26 12:02:55 -0700

    Added test for mixed case email address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   validates :name, presence: true, length: {maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+# OLD
+#  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+# NEW
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   					uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
